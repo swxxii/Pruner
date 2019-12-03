4fd0a3d13d382f089bd4d0a13e1c6119ab411aaf
swxxii <did-wojg@ottermail.app>
2019-12-03 21:13:48 +1100

    fix bug with prune detection, rename var

--- Pruner.ps1
+++ Pruner.ps1
@@ -13,22 +13,24 @@ $PruneAfter = 10
 # Change to the IP and Port of your qBittorrent server
 $Server = "10.0.0.2:8081"
 
+#
 #   MAIN SCRIPT EXECUTION
+#
 
 $OlderThan = Get-Date (Get-Date).AddMinutes($PruneAfter * -1)
 
 $Result = Invoke-RestMethod -Uri http://$Server/query/torrents?filter=completed
 
 foreach ($torrent in $result) {
-    $fin = [timezone]::CurrentTimeZone.ToLocalTime((Get-Date 01.01.1970) + ([System.TimeSpan]::fromseconds($torrent.completion_on)))
-    $prune = ($fin -lt $OlderThan)
-    Write-Output $torrent.name
-    Write-Output ('Hash = ' + $torrent.hash)
-    Write-Output ('Finished on ' + $fin + ' [' + $torrent.completion_on + ']')
+    $Finished = [timezone]::CurrentTimeZone.ToLocalTime((Get-Date 01.01.1970) + ([System.TimeSpan]::fromseconds($torrent.completion_on)))
+    $prune = ($Finished -lt $OlderThan)
+    Write-Output ('Torrent: $torrent.name')
+    Write-Output ('Hash: ' + $torrent.hash)
+    Write-Output ('Finished on ' + $Finished + ' [' + $torrent.completion_on + ']')
     Write-Output "Prune = $prune"
     Write-Output ""
 
-    If ($Fin) {
+    If ($prune) {
         $Body = ('hashes=' + $torrent.hash)
         Invoke-RestMethod -Method Post -Uri http://$Server/command/delete -Body $Body
     }
